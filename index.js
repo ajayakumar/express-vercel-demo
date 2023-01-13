61488af35c7c8c23103e2ad4113641c960513049
Ajay <ajayakumar.s@gmail.com>
2023-01-13 23:09:42 +0530

    vercel confic

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 9000;
 
+app.use("/",(req, res)=>{
+    res.json({message: "Hello From Express App"})
+})
+
 app.listen(port, ()=>{
     console.log(`Starting Server on port: ${port} `);
 })
