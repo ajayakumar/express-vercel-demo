9c9289a6240a0f99550740d238f6b2dabc26b404
Ajay <ajayakumar.s@gmail.com>
2023-01-14 12:47:52 +0530

    added server logic

--- index.js
+++ index.js
@@ -14,12 +14,58 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 9000;
 
 app.use("/",(req, res)=>{
     res.json({message: "Hello From Express App"})
 })
 
+app.post('/', async(req, res)=>{
+
+    try {
+
+        const prompt = req.body.prompt;
+        console.log(prompt);
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+          });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({error})
+    }
+    
+});
+
+/*
 app.listen(port, ()=>{
     console.log(`Starting Server on port: ${port} `);
 })
+*/
+
+try {
+    
+    const port = process.env.PORT || 5557;
+    console.log(port);
+    app.listen({port}, ()=> console.log(`Server is running on port http://localhost:${port}`));
+    
+} catch (error) {
+
+    console.log(error);
+    
+}
