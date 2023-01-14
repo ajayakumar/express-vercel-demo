357d2df8501b6d85116e9ae96186c6fa37786aa7
Ajay <ajayakumar.s@gmail.com>
2023-01-14 12:30:00 +0530

    added imports

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
 
 const app = express();
 const port = 9000;
